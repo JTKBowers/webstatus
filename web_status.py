2b39402a223b35b09eff22922951c6d5350aebfd
Jamie Bowers <james.t.k.bowers@gmail.com>
2016-02-12 17:19:26 +1300

    Fixed web status query, fixed most recent connection/outage queries, added uptime float formatting and date formatting.

--- web_status.py
+++ web_status.py
@@ -16,39 +16,39 @@ def status():
     web_status = {}
 
     # Get the most recent status
-    cur.execute("SELECT status FROM status LIMIT 1;")
+    cur.execute("SELECT status FROM status ORDER BY ts DESC LIMIT 1;")
     web_status['internet'] = cur.fetchone()[0]
 
     # get the time since the last status change
     if web_status['internet']:
-        cur.execute("SELECT now() - ts FROM status WHERE status = false ORDER BY ts DESC LIMIT 1;")
+        cur.execute("SELECT ts FROM status WHERE status = false ORDER BY ts DESC LIMIT 1;")
     else:
-        cur.execute("SELECT now() - ts FROM status WHERE status = true ORDER BY ts DESC LIMIT 1;")
+        cur.execute("SELECT ts FROM status WHERE status = true ORDER BY ts DESC LIMIT 1;")
     time = cur.fetchone()
 
     if time is None:
         web_status['time'] = "forever"
     else:
-        web_status['time'] = time[0]
+        web_status['time'] = time[0].strftime('%c')
 
     # Get the number of updates in the last hour
     cur.execute("SELECT count(status) FROM status WHERE ts > NOW() - INTERVAL '1 hour';")
     hour_count = cur.fetchone()[0]
 
     cur.execute("SELECT count(status) FROM status WHERE status = true AND ts > NOW() - INTERVAL '1 hour';")
-    web_status['hour_uptime'] = 100*cur.fetchone()[0] / hour_count
+    web_status['hour_uptime'] = "{0:.2f}".format(100*cur.fetchone()[0] / hour_count)
 
     # Get the number of updates in the last day
     cur.execute("SELECT count(status) FROM status WHERE ts > NOW() - INTERVAL '1 day';")
     day_count = cur.fetchone()[0]
     cur.execute("SELECT count(status) FROM status WHERE status = true AND ts > NOW() - INTERVAL '1 day';")
-    web_status['day_uptime'] = 100*cur.fetchone()[0] / day_count
+    web_status['day_uptime'] = "{0:.2f}".format(100*cur.fetchone()[0] / day_count)
 
     # Get the number of updates in the last week
     cur.execute("SELECT count(status) FROM status WHERE ts > NOW() - INTERVAL '1 week';")
     week_count = cur.fetchone()[0]
     cur.execute("SELECT count(status) FROM status WHERE status = true AND ts > NOW() - INTERVAL '1 week';")
-    web_status['week_uptime'] = 100* cur.fetchone()[0] / week_count
+    web_status['week_uptime'] = "{0:.2f}".format(100* cur.fetchone()[0] / week_count)
 
     return template.render(web_status)
 
